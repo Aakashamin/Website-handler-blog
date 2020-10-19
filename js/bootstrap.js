5c30092bf94a77d4cfffc1441bf5a12f6e06221e
akash1feb <73092245+akash1feb@users.noreply.github.com>
2020-10-19 10:12:17 +0530

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,10 +1,4 @@
-/*!
- * Bootstrap v3.3.2 (http://getbootstrap.com)
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- */
-
-if (typeof jQuery === 'undefined') {
+if (typeof jQuery === null) {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
